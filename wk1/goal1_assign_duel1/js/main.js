619bcc5983c2b56f38e386a4a1845d6b602af528
Angelica <hernandez.angelica77@gmail.com>
2014-03-05 21:22:57 -0500

    update

--- wk1/goal1_assign_duel1/js/main.js
+++ wk1/goal1_assign_duel1/js/main.js
@@ -1,83 +1,82 @@
 /*
  Name: Angelica Dinh
- Date: Nov. 2, 2013  
- Class: PWA1-01
- Assignment: Goal 1_Duel Fight
-*/
+ Date: March 4, 2014
+ Assignment: Goal 1 -Assignment Duel 1
+ */
 
-//Fight Game
-
-// self-executing function
+//self-executing function
 (function(){
-	
-    console.log("FIGHT!!!");
 
-    //Player Name
-    var playerOneName = "Spiderman";
-    var playerTwoName = "Batman";
+    console.log('Fight!!!');
 
-    //Player Damage
-    var player1Damage = 20;
-    var player2Damage = 20;
+    //player name
+    var player1 = "Spiderman";
+    var player2 = "Batman";
 
-    //Player Health
-    var playerOneHealth = 100;
-    var playerTwoHealth = 100;
+    //player damage
+    var damage1 = 20;
+    var damage2 = 20;
 
-    //Initiate Round
-    var round=0;
+    //player health
+    var health1 = 100;
+    var health2 = 100;
+
+    //initial round
+    var round = 1;
 
     function fight(){
-    	console.log('in the fight function');
-        alert(playerOneName+":"+playerOneHealth+"  *START*  "+playerTwoName+":"+playerTwoHealth);
-        
-        for (var i = 0; i < 10; i++){
-            //random formula is - Math.floor(Math.random() * (max - min) + min);
-            var minDamage1 = player1Damage * .5;
-            var minDamage2 = player2Damage * .5;
-            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
-            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+        console.log('in the fight function');
 
-			console.log(f1);
-			console.log(f2);
-			
-			//Inflict Damage
-            playerOneHealth-=f1;
-            playerTwoHealth-=f2;
+        alert(player1+":"+health1+" *START* "+player2+":"+health2);
 
-            console.log(playerOneHealth);
-            console.log(playerTwoHealth);
-            
-            console.log(playerOneName+": "+playerOneHealth + " " + playerTwoName+":"+playerTwoHealth);
+        for(var i=0; i < 10; i++){
+            //random formula is - Math.floor(Math.random() * (max-min) + min);
+            //Math.random number ranges from 0 to 1
 
-            //check for victor
-            var result = winnerCheck();
-            console.log(result);
-            if (result==="no winner"){
-                round++;
-                alert(playerOneName+":"+playerOneHealth+"  *ROUND "+round+" OVER"+"*  "+playerTwoName+":"+playerTwoHealth);
+            var minDamage1 = damage1 * .5; //min damage should be 1/2 the damage 20 * .5 = 10
+            var minDamage2 = damage2 * .5
+            var f1 = Math.floor(Math.random()*(damage1-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(damage2-minDamage2)+minDamage2);
+
+            console.log(f1);
+            console.log(f2);
+
+            //damage that was inflicted
+            health1 -= f1;
+            health2 -= f2;
+
+            console.log(player1+":" +health1+ " " + player2+":" +health2);
+
+            //at end of each round, need to check for winner so we need to call winnerCheck
+            var results = winnerCheck();
+            console.log(results);
+
+            if(results === "no winner"){
+                round++; //if no winner add another round
+                alert(player1+":"+health1+" *Round "+round+" Over"+"* "+player2+":"+health2);
+
             }else{
-                alert(result);
+                alert(results);
                 break;
             };
+
         };
     };
-
-//use the conditional statement
 
     function winnerCheck(){
-        var result="no winner";
-        if (playerOneHealth<1 && playerTwoHealth<1){
-            result = "You Both Die";
-        }else if(playerOneHealth<1){
-            result =playerTwoName+" WINS!!!"
-        }else if (playerTwoHealth<1){
-            result = playerOneName+" WINS!!!"
+        var result ="no winner";
+
+        if(player1 < 1 && player2 < 1){
+            result="You Both Die";
+        }else if(player1 < 1){
+            result = player2+ "Wins!!";
+        }else if(player2 < 1){
+            result = player1+ "Wins!!";
         };
-       return result;
+        return result;
     };
-//wnt 2 rtn so it rtns info bck 2 the pt where the winner chck was called
-    /*******  Program gets started below *******/
-    fight();
+
+    console.log('program starts')
+    fight(); //calling the fight function...program begins here
 
 })();
